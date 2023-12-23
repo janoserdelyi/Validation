6c5c70a4f5c5c54dc7270513c14d0bc184d049b1
janos erdelyi <janos.erdelyi@gmail.com>
2023-12-23 13:56:53 -0500

    moving ResultExtensions into own file in hopes of working around a possible bug

--- src/Result.cs
+++ src/Result.cs
@@ -75,36 +75,3 @@ public class Result<T>
 		return Failure<TValue> ((int)Error.Empty, "No value provided");
 	}
 }
-
-public static class ResultExtensions
-{
-	public static Result<T> Ensure<T> (
-		this Result<T> result,
-		Func<T?, bool> predicate,
-		int error,
-		string? errorMessage = null
-	) {
-		if (result.IsFailure == true) {
-			return result;
-		}
-
-		if (predicate (result.Value) == true) {
-			return result;
-		}
-
-		// return a new result with the error since this was not successful
-		return Result<T>.Failure<T> (error, errorMessage);
-	}
-
-	// we need to map from one thing to another for final outputs
-	public static Result<TOut> Map<TIn, TOut> (
-		this Result<TIn> result,
-		Func<TIn?, TOut> mappingFunc
-	) {
-		if (result.IsSuccess) {
-			return Result<TIn>.Success (mappingFunc (result.Value));
-		}
-
-		return Result<TOut>.Failure<TOut> (result.ErrorCode, result.ErrorMessage);
-	}
-}
